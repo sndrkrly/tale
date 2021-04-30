ec2b7560edba2f86e289abb49641611a92a750a5
Sandor Kiraly <48065581+sndrkrly@users.noreply.github.com>
2021-04-30 19:15:12 +0200

    Update index.tsx

--- kibbeh/src/pages/index.tsx
+++ kibbeh/src/pages/index.tsx
@@ -1,3 +1,10 @@
+/*
+    Created by Sándor Király on 30/04/21.
+
+    Copyright (c) OMEGA Magyarország Kft.
+    All rights reserved.
+*/
+
 import { Landing } from '../modules/landing/Landing'; 
 
 export default Landing;
