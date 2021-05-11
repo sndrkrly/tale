6714a3313dee271e25821ffcdbb3623523e9a41f
Sandor Kiraly <48065581+sndrkrly@users.noreply.github.com>
2021-05-11 08:00:46 +0200

    Add files via upload

--- kibbeh/README.md
+++ kibbeh/README.md
@@ -1,3 +1,13 @@
 <h1 align="left">
 	✨ Next.js (Frontend)
 </h1>
+
+export const Navbar = () => {
+    return (
+        <>
+            <h1 className = 'text-primary-100'>
+                header
+            </h1>
+        </>
+    );
+}
--- kibbeh/tailwind.config.js
+++ kibbeh/tailwind.config.js
@@ -1,125 +1,133 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
-  purge: [
-    './src/pages/**/*/.js',
-    //'./src/components/**/*.js'
-    './public/index.html',
-  ],
+    purge: [
+        '.src/pages/**/*/.js', 
+        './public/index.html'
+    ],
 
-  darkMode: false, // or 'media' or 'class'
-  
-  theme: {
-    fontFamily: {
-      sans: [
-        "Inter",
-        "-apple-system",
-        "BlinkMacSystemFont",
-        "Segoe UI",
-        "Roboto",
-        "Helvetica",
-        "Arial",
-        "sans-serif",
-      ],
-      mono: ["Menlo", "Monaco", "Courier New", "monospace"],
-    },
-
-    fontSize: {
-      tiny: "0.625rem",
-      xs: ".75rem",
-      sm: ".875rem",
-      base: "1rem",
-      lg: "1.125rem",
-      xl: "1.25rem",
-      "2xl": "1.5rem",
-      "3xl": "1.875rem",
-      "4xl": "2.25rem",
-      "5xl": "3rem",
-      "6xl": "4rem",
-      "7xl": "5rem",
-    },
-
-    colors: {
-      button: "var(--color-button-text)",
-      transparent: "transparent",
-      primary: {
-        100: "var(--color-primary-100)",
-        200: "var(--color-primary-200)",
-        300: "var(--color-primary-300)",
-        600: "var(--color-primary-600)",
-        700: "var(--color-primary-700)",
-        800: "var(--color-primary-800)",
-        900: "var(--color-primary-900)",
-      },
-      secondary: {
-        DEFAULT: "var(--color-secondary)",
-        "washed-out": "var(--color-secondary-washed-out)",
-      },
-      accent: {
-        DEFAULT: "var(--color-accent)",
-        hover: "var(--color-accent-hover)",
-        disabled: "var(--color-accent-disabled)",
-      },  
-    },
-
-    spacing: {
-      0: "0px",
-      1: "5px",
-      1.5: "6px",
-      2: "10px",
-      3: "15px",
-      4: "20px",
-      5: "30px",
-      5.5: "35px",
-      6: "40px",
-      6.5: "50px",
-      7: "60px",
-      8: "75px",
-      9: "80px",
-      10: "90px",
-      11: "100px",
-      15: "150px",
-      "5l": "10rem",
-      "n1/2": "-50%",
-      24: "24rem",
-      400: "400px",
-    },
-
-    boxShadow: {
-      outlineLg: "0 0 0 4pt var(--color-primary-800)",
-      outlineMd: "0 0 0 2pt var(--color-primary-800)",
-      outlineSm: "0 0 0 1pt var(--color-primary-800)",
-    },
+    darkMode: 'class',
     
-    borderWidth: {
-      DEFAULT: "1px",
-      0: "0px",
-      4: "4px",
-      2: "2px",
+    theme: {
+        fontFamily: {
+            sans: [
+                'Inter',
+                '-apple-system',
+                'BlinkMacSystemFont',
+                'Segoe UI',
+                'Roboto',
+                'Helvetica',
+                'Arial',
+                'sans-serif',
+            ],
+
+            mono: [
+                'Menlo',
+                'Monaco',
+                'Courier New',
+                'monospace',
+            ],
+        },
+
+        fontSize: {
+            tiny: '0.625rem',
+            xs: '.75rem',
+            sm: '.875rem',
+            base: '1rem',
+            lg: '1.125rem',
+            xl: '1.25rem',
+            '2xl': '1.5rem',
+            '3xl': '1.875rem',
+            '4xl': '2.25rem',
+            '5xl': '3rem',
+            '6xl': '4rem',
+            '7xl': '5rem',            
+        },
+
+        colors: {
+            white: colors.white,
+            black: colors.black,
+            blueGray: colors.blueGray,
+            coolGray: colors.coolGray,
+            gray: colors.gray,
+            trueGray: colors.trueGray,
+            warmGray: colors.warmGray,
+            red: colors.red,
+            orange: colors.orange,
+            amber: colors.amber,
+            yellow: colors.yellow,
+            lime: colors.lime,
+            green: colors.green,
+            emerald: colors.emerald,
+            teal: colors.teal,
+            cyan: colors.cyan,
+            lightBlue: colors.lightBlue,
+            blue: colors.blue,
+            indigo: colors.indigo,
+            violet: colors.violet,
+            purple: colors.purple,
+            fuchsia: colors.fuchsia,
+            pink: colors.pink,
+            rose: colors.rose,            
+        },
+
+        spacing: {
+            0: '0px',
+            1: '5px',
+            1.5: '6px',
+            2: '10px',
+            3: '15px',
+            4: '20px',
+            5: '30px',
+            5.5: '35px',
+            6: '40px',
+            6.5: '50px',
+            7: '60px',
+            8: '75px',
+            9: '80px',
+            10: '90px',
+            11: '100px',
+            15: '150px',
+            15.5: '180px',
+            18: '183px',
+            19: '195px',
+            20: '210px',
+            21: '15rem',
+            24: '12rem',
+            400: '400px',
+            600: '600px',
+            635: '635px',
+            850: '850px',
+            '5l': '10rem',
+            '55l': '285px',
+            '6l': '300px',
+            '7l': '320px',
+            'n1/2': '-50%',
+        },
+          
+        borderWidth: {
+            DEFAULT: '1px',
+            0: '0px',
+            4: '4px',
+            2: '2px',
+        },
+
+        borderRadius: {
+            5: '5px',
+            8: '8px',
+            40: '40px',
+        },
+
+        extend: {
+            outline: {
+                'no-chrome': 'none',
+            },
+        },
     },
 
-    extend: {
-      borderRadius: {
-        5: "5px",
-        8: "8px",
-        40: "40px",
-      },
-      borderColor: {
-        "color-800": "var(--color-primary-800)",
-      },
-      outline: {
-        "no-chrome": "none",
-      },
-      transitionTimingFunction: {
-        "in-out-hard": "cubic-bezier(.77, 0, .175, 1)",
-      },
-      transitionDuration: {
-        400: "400ms",
-      },
+    variants: {
+        extends: {},
     },
-  },
 
-  variants: {
-    extend: {},
-  },
-  
-  plugins: [],
+    plugins: []
 }
