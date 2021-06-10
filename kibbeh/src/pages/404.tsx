4875a7712b7135750796bfa1b86c6deedbec243e
Sandor Kiraly <48065581+sndrkrly@users.noreply.github.com>
2021-06-10 16:58:05 +0200

    Add files via upload

--- kibbeh/src/pages/404.tsx
+++ kibbeh/src/pages/404.tsx
@@ -15,15 +15,26 @@ export default function Custom404() {
                 <link rel = 'icon' href = '/favicon.ico' />
             </Head>
 
-            <main className = 'flex grid w-full h-full'>
-                <div className = 'flex items-center justify-center min-w-screen min-h-screen bg-white dark:bg-coolGray-900'>
-                    <h1 className = 'font-extrabold text-black dark:text-white sm:text-5xl'>
-                        404
-                    </h1>
+            <main className = 'bg-white dark:bg-coolGray-900 flex grid w-full h-full'>
+                <div className = 'flex items-center justify-center min-w-screen min-h-screen'>
+                    <div className = 'flex m-auto flex-col p-6 gap-5 sm:rounded-8 z-10 sm:w-400 w-400'>
+                        <div className = 'flex gap-2 flex-col text-left'>
+                            <h1 className = 'font-extrabold text-black dark:text-coolGray-100 sm:text-3xl'>
+                               <img src = 'https://i.imgur.com/GBOgfkc.png' width = '32' height = '32'/> Nem található.
+                            </h1>
 
-                    <p className = 'font-normal text-black dark:text-white sm:text-3xl ml-5'>
-                        Az oldal nem található.
-                    </p>
+                            <p className = 'text-gray-500 dark:text-coolGray-100 flex-wrap text-base'>
+                                A keresett oldal nem található.
+                            </p> 
+
+                            <div className = 'mt-2'>
+                                <div className = 'py-1 text-gray-500 dark:text-gray-400 font-normal text-xs'>
+                                    <p>OMEGA Magyarország Kft. © 2021.</p>
+                                    <p>Minden jog fenntartva.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </main>
         </>
