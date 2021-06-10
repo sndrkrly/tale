e95bf5018524681b1d21a1f1ce0c266b43eed4c7
Sandor Kiraly <48065581+sndrkrly@users.noreply.github.com>
2021-06-10 16:58:36 +0200

    Add files via upload

--- kibbeh/src/modules/auth/Login.tsx
+++ kibbeh/src/modules/auth/Login.tsx
@@ -44,9 +44,9 @@ export const Login = () => {
             <main className = 'bg-white dark:bg-coolGray-900 flex grid w-full h-full'>
                 <div className = 'flex items-center justify-center min-w-screen min-h-screen'>
                     <div className = 'flex m-auto flex-col p-6 gap-5 sm:rounded-8 z-10 sm:w-400 w-400'>
-                        <div className = 'flex gap-2 flex-col text-center'>
+                        <div className = 'flex gap-2 flex-col text-left'>
                             <h1 className = 'font-extrabold text-black dark:text-coolGray-100 sm:text-3xl mb-4'>
-                                Bejelentkezés.
+                               <img src = 'https://i.imgur.com/GBOgfkc.png' width = '32' height = '32'/> Bejelentkezés.
                             </h1>
 
                             <p className = 'text-gray-500 dark:text-coolGray-100 flex-wrap text-base'>
@@ -102,6 +102,11 @@ export const Login = () => {
                                 </a>
                             </Link>
                         </small>
+
+                        <div className = 'py-1 text-gray-500 dark:text-gray-400 font-normal text-xs'>
+                            <p>OMEGA Magyarország Kft. © 2021.</p>
+                            <p>Minden jog fenntartva.</p>
+                        </div>
                     </div>
                 </div>
             </main>
